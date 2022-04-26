935ab5b35f4121a923a55c83a50b1c4514444929
sagiv hazut <sagivhazut@gmail.com>
2022-04-26 10:25:56 +0300

    first commit

---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,102 @@
+import React, { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  IconButton,
+  Badge,
+  MenuItem,
+  Menu,
+  Typography,
+} from "@material-ui/core";
+import { ShoppingCart } from "@material-ui/icons";
+import { Link } from "react-router-dom";
+
+import logo from "../../assets/log.webp";
+import useStyles from "./styles";
+
+const PrimarySearchAppBar = ({ totalItems }) => {
+  const [mobileMoreAnchorEl, setMobileMoreAnchorEl] = useState(null);
+  const classes = useStyles();
+
+  const isMobileMenuOpen = Boolean(mobileMoreAnchorEl);
+
+  const handleMobileMenuClose = () => setMobileMoreAnchorEl(null);
+
+  const mobileMenuId = "primary-search-account-menu-mobile";
+
+  const renderMobileMenu = (
+    <Menu
+      anchorEl={mobileMoreAnchorEl}
+      anchorOrigin={{ vertical: "top", horizontal: "right" }}
+      id={mobileMenuId}
+      keepMounted
+      transformOrigin={{ vertical: "top", horizontal: "right" }}
+      open={isMobileMenuOpen}
+      onClose={handleMobileMenuClose}
+    >
+      <MenuItem>
+        <IconButton
+          component={Link}
+          to="/cart"
+          aria-label="Show cart items"
+          color="inherit"
+        >
+          <Badge badgeContent={totalItems} color="secondary">
+            <ShoppingCart />
+          </Badge>
+        </IconButton>
+        <p>Cart</p>
+      </MenuItem>
+    </Menu>
+  );
+
+  return (
+    <>
+      <AppBar position="fixed" className={classes.appBar} color="inherit">
+        <Toolbar>
+          <Typography
+            component={Link}
+            to="/home"
+            variant="h6"
+            className={classes.title}
+            color="inherit"
+          >
+            <img
+              src={logo}
+              alt="Niconor"
+              height="25px"
+              className={classes.image}
+            />{" "}
+            Niconor
+          </Typography>
+          <Typography
+            component={Link}
+            to="/products"
+            variant="h6"
+            className={classes.title}
+            color="inherit"
+          >
+            Store
+          </Typography>
+          <div className={classes.grow} />
+
+          <div className={classes.button}>
+            <IconButton
+              component={Link}
+              to="/cart"
+              aria-label="Show cart items"
+              color="inherit"
+            >
+              <Badge badgeContent={totalItems} color="secondary">
+                <ShoppingCart />
+              </Badge>
+            </IconButton>
+          </div>
+        </Toolbar>
+      </AppBar>
+      {renderMobileMenu}
+    </>
+  );
+};
+
+export default PrimarySearchAppBar;
---
+++ src/components/pages/Homepage.jsx
@@ -0,0 +1,195 @@
+import { Fragment } from "react";
+import { NavLink } from "react-router-dom";
+import React from "react";
+import "./Homepage.css";
+
+import log1 from "../../assets/gate1.jpg";
+import log2 from "../../assets/gate2.jpg";
+import log3 from "../../assets/gate3.jpg";
+import { Carousel } from "react-responsive-carousel";
+import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+
+//https://www.somfy.co.il/?utm_source=search&utm_medium=brand&utm_campaign=bny2022&gclid=Cj0KCQjwjN-SBhCkARIsACsrBz5u3Bh9mhPBGuS3UvM06wJkqbcsLbBiRwPwMWt5LLtajYYSEJIeYWAaAj-pEALw_wcB
+const HomePage = () => {
+  return (
+    <Fragment>
+      <div>
+        <Carousel
+          className="main-slide"
+          infiniteLoop={true}
+          autoPlay={true}
+          interval={5000}
+          showStatus={false}
+          dynamicHeight={true}
+          showThumbs={false}
+        >
+          <div>
+            <div className="imageone">
+              <h1 className="header">
+                שעריים חשמליים ממונעים
+                <br />
+                <NavLink
+                  className="nav-links"
+                  aria-current="page"
+                  to="/products"
+                  activeClassName="activeLink"
+                >
+                  לפרטים נוספים
+                </NavLink>
+              </h1>
+              <img src={log1} alt="" width="90vw" height="880vh" />
+
+              <p className="legend">Legend 1</p>
+            </div>
+          </div>
+          <div className="imageone">
+            <h1 className="header">
+              שעריים חשמליים ממונעים
+              <NavLink
+                className="nav-links"
+                aria-current="page"
+                to="/products"
+                activeClassName="activeLink"
+              >
+                לפרטים נוספים
+              </NavLink>
+            </h1>
+            <img src={log2} alt="" width="90vw" height="880vh" />
+
+            <p className="legend">Legend 2</p>
+          </div>
+          <div className="imageone">
+            <h1 className="header">
+              שעריים חשמליים ממונעים
+              <NavLink
+                className="nav-links"
+                aria-current="page"
+                to="/products"
+                activeClassName="activeLink"
+              >
+                לפרטים נוספים
+              </NavLink>
+            </h1>
+
+            <img src={log3} alt="" width="90vw" height="880vh" />
+            <p className="legend">Legend 3</p>
+          </div>
+        </Carousel>
+        <br />
+        <div className="somfey">
+          <h1>פתרונות לבית חכם מבית Somfy, השותפים שלכם בבית</h1>
+        </div>
+        <div className="images">
+          <div className="imagesfirstrow">
+            <div class="container">
+              <img src={log3} alt="" className="image"></img>
+              <div class="overlay">
+                <div class="text">
+                  <p> Hello World</p>
+                  <NavLink
+                    className="nav-links"
+                    aria-current="page"
+                    to="/products"
+                    activeClassName="activeLink"
+                  >
+                    לפרטים נוספים
+                  </NavLink>
+                </div>
+              </div>
+            </div>
+            <div class="container">
+              <img src={log3} alt="" className="image"></img>
+              <div class="overlay">
+                <div class="text">
+                  {" "}
+                  <p> Hello World</p>
+                  <NavLink
+                    className="nav-links"
+                    aria-current="page"
+                    to="/products"
+                    activeClassName="activeLink"
+                  >
+                    לפרטים נוספים
+                  </NavLink>
+                </div>
+              </div>
+            </div>{" "}
+            <div class="container">
+              <img src={log3} alt="" className="image"></img>
+              <div class="overlay">
+                <div class="text">
+                  {" "}
+                  <p> Hello World</p>
+                  <NavLink
+                    className="nav-links"
+                    aria-current="page"
+                    to="/products"
+                    activeClassName="activeLink"
+                  >
+                    לפרטים נוספים
+                  </NavLink>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          <div className="imagesfirstrow">
+            <div class="container">
+              <img src={log3} alt="" className="image"></img>
+              <div class="overlay">
+                <div class="text">
+                  {" "}
+                  <p> Hello World</p>
+                  <NavLink
+                    className="nav-links"
+                    aria-current="page"
+                    to="/products"
+                    activeClassName="activeLink"
+                  >
+                    לפרטים נוספים
+                  </NavLink>
+                </div>
+              </div>
+            </div>
+            <div class="container">
+              <img src={log3} alt="" className="image"></img>
+              <div class="overlay">
+                <div class="text">
+                  {" "}
+                  <p> Hello World</p>
+                  <NavLink
+                    className="nav-links"
+                    aria-current="page"
+                    to="/products"
+                    activeClassName="activeLink"
+                  >
+                    לפרטים נוספים
+                  </NavLink>
+                </div>
+              </div>
+            </div>{" "}
+            <div class="container">
+              <img src={log3} alt="" className="image"></img>
+              <div class="overlay">
+                <div class="text">
+                  {" "}
+                  <p> Hello World</p>
+                  <NavLink
+                    className="nav-links"
+                    aria-current="page"
+                    to="/products"
+                    activeClassName="activeLink"
+                  >
+                    לפרטים נוספים
+                  </NavLink>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Fragment>
+  );
+};
+
+export default HomePage;
